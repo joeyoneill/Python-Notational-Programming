3b81f4ded252b8c4b9a73cc66cc62544c75c15c7
wslominski <59939066+wslominski@users.noreply.github.com>
2022-12-02 19:06:37 -0500

    Add files via upload

--- Python-Notational-Programming/PythonNotationalProgramming.py
+++ Python-Notational-Programming/PythonNotationalProgramming.py
@@ -1,17 +1,29 @@
 import os, sys, io, time
 import shutil, logging
+import subprocess
 
 def read_text():
+    txt = []
     captions = os.listdir('Text')
     for caption in captions:
         if caption.startswith('.'):
             continue;
         caption_file = caption
         with io.open("Text/"+caption_file) as cf:
-            txt = cf.read()
-            #print(txt)
-        return str(txt)
+            for i, line in enumerate(cf):
+                txt.append(line.strip())
+        return txt
         break;
+
+def write_python_file(dir, text):
+    os.chdir(cwd+"/"+dir)
+    f = open("Output.py","w+")
+    f.write("if __name__ == '__main__':")
+    f.write('\n'+'\t')
+    for t in text:
+        f.write(t)
+        f.write('\n'+'\t')
+    f.close()
 
 if __name__ == '__main__':
     # Get Current Working Directory
@@ -23,5 +35,11 @@ if __name__ == '__main__':
     # Come Back To Original Directory
     os.chdir(cwd)
 
-    # Print Text File
-    print(text)
+    # Create Text File and Paste Copied Input
+    write_python_file('Output',text)
+
+    # While In The Output Directory, Run The File Just Created
+    subprocess.call(" python Output.py 1", shell = True)
+
+    # Come back To Original Directory
+    os.chdir(cwd)
